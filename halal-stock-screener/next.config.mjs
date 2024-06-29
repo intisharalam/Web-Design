30c4111bea192f9c7d7b3675499b3eb99e64ebbd
Intishar <49362508+intisharalam@users.noreply.github.com>
2024-06-29 12:29:56 +0100

    Revert "Vercel Deployment error fix - 1"
    This reverts commit da69207e7f102e9f9bcaee59478abe2445a97445.

--- halal-stock-screener/next.config.mjs
+++ halal-stock-screener/next.config.mjs
@@ -1,25 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-
-
-// next.config.mjs
 export default {
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/home',
-                permanent: true, // or false if it's a temporary redirect
-            },
-        ];
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:8000/api/:path*', // Proxy to FastAPI backend
-        },
-      ];
-    },
+  ...nextConfig,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // or false if it's a temporary redirect
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*', // Proxy to FastAPI backend during development
+      },
+    ];
+  },
 };
