89cd5a962d6291dc2e1556ebd5ee0ceb4aa3c8c9
Intishar <49362508+intisharalam@users.noreply.github.com>
2024-06-29 19:48:21 +0100

    stuff

--- halal-stock-screener/app/layout.tsx
+++ halal-stock-screener/app/layout.tsx
@@ -1,7 +1,12 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.scss";
+<<<<<<< HEAD
 import Navbar from '../_components/navbar.tsx'
 import Footer from "../_components/footer.tsx";
+=======
+import Navbar from '../components/navbar'
+import Footer from "../components/footer";
+>>>>>>> parent of 8e2a743 (stuff)
 
 const inter = Inter({ subsets: ["latin"] });
 
