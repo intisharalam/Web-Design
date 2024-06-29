269cc36f4524f15857ec15776e384d1ba4e887af
Intishar <49362508+intisharalam@users.noreply.github.com>
2024-06-29 18:50:16 +0100

    Update next.config.mjs

--- halal-stock-screener/next.config.mjs
+++ halal-stock-screener/next.config.mjs
@@ -9,6 +9,11 @@ export default {
                 destination: '/home',
                 permanent: true, // or false if it's a temporary redirect
             },
+            {
+                source: "/api/:path*",
+                destination: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/:path*" : "/api-handler/:path*",
+                permanent: true,
+            },
         ];
     },
 };
