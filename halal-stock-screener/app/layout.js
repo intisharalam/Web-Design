bbdd07e40f7923984808ec3b85de0bb747e671b3
Intishar <49362508+intisharalam@users.noreply.github.com>
2024-06-29 19:56:10 +0100

    changes

--- halal-stock-screener/app/layout.js
+++ halal-stock-screener/app/layout.js
@@ -1,27 +1,8 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.scss";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD:halal-stock-screener/app/layout.tsx
-<<<<<<< HEAD
-import Navbar from '../_components/navbar.tsx'
-import Footer from "../_components/footer.tsx";
-=======
+
 import Navbar from '../components/navbar'
 import Footer from "../components/footer";
->>>>>>> parent of 8e2a743 (stuff)
-=======
-import Navbar from '../_components/navbar'
-import Footer from "../_components/footer";
->>>>>>> parent of 107855b (changed some formatting):halal-stock-screener/app/layout.js
-=======
-import Navbar from '../_components/navbar'
-import Footer from "../_components/footer";
->>>>>>> parent of 107855b (changed some formatting)
-=======
-import Navbar from '../_components/navbar'
-import Footer from "../_components/footer";
->>>>>>> parent of 107855b (changed some formatting)
 
 const inter = Inter({ subsets: ["latin"] });
 
