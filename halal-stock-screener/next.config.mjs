8ab30309a17ea9d28db8c4b90332808f25331a04
Intishar <49362508+intisharalam@users.noreply.github.com>
2024-06-29 12:34:22 +0100

    fixing vercel issues

--- halal-stock-screener/next.config.mjs
+++ halal-stock-screener/next.config.mjs
@@ -1,23 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+
+
+// next.config.mjs
 export default {
-  ...nextConfig,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true, // or false if it's a temporary redirect
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*', // Proxy to FastAPI backend during development
-      },
-    ];
-  },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true, // or false if it's a temporary redirect
+            },
+        ];
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:8000/api/:path*', // Proxy to FastAPI backend
+        },
+      ];
+    },
 };
